426fcbafc79725dcfb53339b5078c8169a2c60a8
newuser <newuser@newusers-iMac.home>
2015-05-16 20:27:48 -0400

    Initial Commit

---
+++ My Visits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Visits
+//
+//  Created by newuser on 5/16/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
